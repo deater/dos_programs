1d3cdaf2abfadf424a5c710c295fe6dcb5579ef4
Vince Weaver <vince@deater.net>
2026-02-26 01:21:36 -0500

    Update text parsing

---
+++ games/peasant/text/shrink_text.c
@@ -0,0 +1,204 @@
+#include <stdio.h>
+#include <string.h>
+
+#define NUM_WORDS 75
+
+static char word_list[NUM_WORDS][20]={
+	"already","cottage","peasant","There's","Trogdor",
+	"little","you're","You're",
+	"about","can't","don't","looks","there","There","those",
+	"baby","dead","from","game","good","have","here","into","it's","It's","just","like","over","says","some","that","That","this","with","your",
+	"all","and","are","but","for","get","got","him","his","not","now","old","one","out","see","the","The","was","you","You",
+	"an","at","be","do","go","he","He","in","is","it","It","my","no","No","of","on","or","so","to","up",
+};
+
+static char replacement_list[NUM_WORDS][32]={
+	"DIALOG_ALREADY",
+	"DIALOG_COTTAGE",
+	"DIALOG_PEASANT",
+	"DIALOG_CAPITAL_THERES",
+	"DIALOG_CAPITAL_TROGDOR",
+
+	"DIALOG_LITTLE",
+	"DIALOG_YOURE",
+	"DIALOG_CAPITAL_YOURE",
+
+	"DIALOG_ABOUT",
+	"DIALOG_CANT",
+	"DIALOG_DONT",
+	"DIALOG_LOOKS",
+	"DIALOG_THERE",
+	"DIALOG_CAPITAL_THERE",
+	"DIALOG_THOSE",
+
+	"DIALOG_BABY",
+	"DIALOG_DEAD",
+	"DIALOG_FROM",
+	"DIALOG_GAME",
+	"DIALOG_GOOD",
+	"DIALOG_HAVE",
+	"DIALOG_HERE",
+	"DIALOG_INTO",
+	"DIALOG_ITS",
+	"DIALOG_CAPITAL_ITS",
+	"DIALOG_JUST",
+	"DIALOG_LIKE",
+	"DIALOG_OVER",
+	"DIALOG_SAYS",
+	"DIALOG_SOME",
+	"DIALOG_THAT",
+	"DIALOG_CAPITAL_THAT",
+	"DIALOG_THIS",
+	"DIALOG_WITH",
+	"DIALOG_YOUR",
+
+	"DIALOG_ALL",
+	"DIALOG_AND",
+	"DIALOG_ARE",
+	"DIALOG_BUT",
+	"DIALOG_FOR",
+	"DIALOG_GET",
+	"DIALOG_GOT",
+	"DIALOG_HIM",
+	"DIALOG_HIS",
+	"DIALOG_NOT",
+	"DIALOG_NOW",
+	"DIALOG_OLD",
+	"DIALOG_ONE",
+	"DIALOG_OUT",
+	"DIALOG_SEE",
+	"DIALOG_THE",
+	"DIALOG_CAPITAL_THE",
+	"DIALOG_WAS",
+	"DIALOG_YOU",
+	"DIALOG_CAPITAL_YOU",
+
+	"DIALOG_AN",
+	"DIALOG_AT",
+	"DIALOG_BE",
+	"DIALOG_DO",
+	"DIALOG_GO",
+	"DIALOG_HE",
+	"DIALOG_CAPITAL_HE",
+	"DIALOG_IN",
+	"DIALOG_IS",
+	"DIALOG_IT",
+	"DIALOG_CAPITAL_IT",
+	"DIALOG_MY",
+	"DIALOG_NO",
+	"DIALOG_CAPITAL_NO",
+	"DIALOG_OF",
+	"DIALOG_ON",
+	"DIALOG_OR",
+	"DIALOG_SO",
+	"DIALOG_TO",
+	"DIALOG_UP",
+
+};
+
+
+static int char_count=0;
+
+void parse_line(char *string) {
+	int len;
+	int i,j;
+	int in_quote=0;
+
+	len=strlen(string);
+
+	/* if line ends in : then label */
+	/* want to grab it and offset */
+	if ( (strstr(string,":")) && (!strstr(string,"\""))) {
+		fprintf(stderr,"%d %s",char_count,string);
+	}
+
+	if (string[0]==';') return;
+
+	for(i=0;i<len;i++) {
+
+		if (!in_quote) {
+			if (string[i]=='\"') {
+				in_quote=1;
+				continue;
+			}
+		}
+		else {
+			/* in quote */
+			if (string[i]=='\"') {
+				in_quote=0;
+				continue;
+			}
+		}
+
+		/* handle carriage return */
+		if (!strncmp(&string[i],",13",3)) {
+			printf("%c",13);
+			i+=3;
+			char_count++;
+			continue;
+		}
+
+		/* handle end of string */
+		if (!strncmp(&string[i],",0",2)) {
+			printf("%c",0);
+			i+=2;
+			char_count++;
+			continue;
+		}
+
+
+		/* handle word substitution */
+		int word_found=0;
+
+		for(j=0;j<NUM_WORDS;j++) {
+			if (!strncmp(&string[i],word_list[j],
+					strlen(word_list[j]))) {
+
+//					printf("MATCH %s%d %s%d\n",
+//						&string[i],i,word_list[j],j);
+
+				printf("%c",0x80+j);
+				char_count+=1;
+				i+=strlen(word_list[j]);
+				word_found=1;
+				break;
+			}
+		}
+
+		if ((!word_found) && (in_quote)) {
+			printf("%c",string[i]);
+			char_count++;
+		}
+	}
+
+
+
+
+
+}
+
+int main(int argc, char **argv) {
+
+	char string[BUFSIZ];
+	char *result;
+	int length_of_table=0;
+	int i;
+
+	for(i=0;i<NUM_WORDS;i++) {
+		length_of_table+=strlen(word_list[i]);
+	}
+
+	while(1) {
+
+		result=fgets(string,BUFSIZ,stdin);
+		if (result==NULL) break;
+
+		parse_line(string);
+
+	}
+
+	fprintf(stderr,"; Done!  rough char count = %d, "
+		"length_of_table = %d\n",
+		char_count, length_of_table);
+
+}
