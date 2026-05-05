1424bf637c738bcb468212b3f88b39ca0b9717a9
Vince Weaver <vincent.weaver@maine.edu>
2026-05-05 16:49:09 -0400

    peasant: hook up some more dialog

--- games/peasant/larch.pas
+++ games/peasant/larch.pas
@@ -2,22 +2,204 @@
 	{ ARCHERY ACTIONS                     }
 	{=====================================}
 
+Procedure archery_play_game(skip: boolean);
+
+begin
+	if (skip<>true) then begin
+
+		if (game_state.ARROW_BEATEN) then begin
+
+		end;
+
+		if (game_state.TRINKET_GIVEN) then begin
+
+		end;
+
+	end;
+
+	print_offset:=
+		archery_dialog(archery_play_game_message2);
+	partial_message_step;
+
+	update_map_location(LOCATION_ARCHERY_GAME);
+
+end;
+
+
 Procedure archery_actions;
 
 begin
 
+	if (current_verb=VERB_GET) or
+		(current_verb=VERB_STEAL) or
+		(current_verb=VERB_TAKE) then begin
+
+		if (current_noun=NOUN_TARGET) then
+			print_offset:=
+				archery_dialog(archery_get_target_message);
+		if (current_noun=NOUN_ARROW) then
+			print_offset:=
+				archery_dialog(archery_get_arrow_message);
+	end;
+
+
 	case current_verb of
+
+	VERB_ASK:	begin
+
+		if (game_state.HALDO_TO_DONGOLEV) then begin
+
+			print_offset:=archery_dialog(archery_ask_about_unknown_message);
+
+			if (current_noun=NOUN_FIRE) then begin
+				print_offset:=
+				archery_dialog(archery_ask_about_fire_message);
+				partial_message_step;
+				print_offset:=
+				archery_dialog(archery_ask_about_fire_message2);
+				partial_message_step;
+				print_offset:=
+				archery_dialog(archery_ask_about_fire_message3);
+				partial_message_step;
+				print_offset:=
+				archery_dialog(archery_ask_about_fire_message4);
+
+			end;
+
+			if (current_noun=NOUN_NED) then
+				print_offset:=
+				archery_dialog(archery_ask_about_ned_message);
+			if (current_noun=NOUN_SMELL) then
+				print_offset:=
+				archery_dialog(archery_ask_about_smell_message);
+			if (current_noun=NOUN_ROBE) then
+				print_offset:=
+				archery_dialog(archery_ask_about_robe_message);
+			if (current_noun=NOUN_TROGDOR) then begin
+				print_offset:=
+				archery_dialog(archery_ask_about_trogdor_message);
+				partial_message_step;
+				print_offset:=
+				archery_dialog(archery_ask_about_trogdor_message2);
+				partial_message_step;
+				print_offset:=
+				archery_dialog(archery_ask_about_trogdor_message3);
+				partial_message_step;
+				print_offset:=
+				archery_dialog(archery_ask_about_trogdor_message4);
+				partial_message_step;
+				print_offset:=
+				archery_dialog(archery_ask_about_trogdor_message5);
+				partial_message_step;
+				print_offset:=
+				archery_dialog(archery_ask_about_trogdor_message6);
+
+			end;
+
+
+		end; { said haldo }
+
+		end; { verb_ask }
+
+	VERB_GIVE:	begin
+
+			{ only if dongolev there }
+			if (current_noun=NOUN_TRINKET)
+				and (game_state.HALDO_TO_DONGOLEV) then begin
+
+				if (game_state.TRINKET_GIVEN) then begin
+					{ already gave it }
+					print_offset:=
+					archery_dialog(archery_give_trinket_again_message);
+				end
+				else if (inventory[ord(TRINKET)]) then begin
+					{ score 2 points }
+					score_points(2);
+
+					{ set trinket given }
+					game_state.TRINKET_GIVEN:=true;
+
+					{ mark us no longer having it }
+					inventory_gone[ord(TRINKET)]:=true;
+
+
+					print_offset:=
+					archery_dialog(archery_give_trinket_message);
+					partial_message_step;
+
+					{ skip part of it }
+					archery_play_game(true);
+
+				end;
+			end;
+
+		end; { verb_give }
+
+	VERB_HALDO:	begin
+
+			if (game_state.HALDO_TO_DONGOLEV) then begin
+				print_offset:=
+					archery_dialog(archery_haldo_message);
+			end;
+		end; { verb_haldo }
 
 	VERB_LOOK:	begin
 
-		if (current_noun=NOUN_TREE) then
-			print_offset:=yellow_tree_dialog(yellow_tree_look_tree_message);
-		if (current_noun=NOUN_COTTAGE) then
-			print_offset:=yellow_tree_dialog(yellow_tree_look_cottage_message);
+		{ TODO: should we say the otherwise case }
+		{ as the default once we've talked to dongolev }
+
+		if (current_noun=NOUN_DESK) then
+			print_offset:=archery_dialog(archery_look_at_desk_message);
+		if (current_noun=NOUN_TARGET) then
+			print_offset:=archery_dialog(archery_look_at_target_message);
+		if (current_noun=NOUN_ARCHER) then begin
+
+			{ first check if we've talked to mendelev }
+			if (game_state.TALKED_TO_MENDELEV<>true) then begin
+				print_offset:=archery_dialog(archery_look_at_archer_message);
+			end else begin
+				{ have talked to mendelev }
+				if (game_state.HALDO_TO_DONGOLEV) then begin
+					{ have talked to dongolev }
+					print_offset:=archery_dialog(archery_look_at_archer_otherwise_message);
+				end else begin
+					print_offset:=archery_dialog(archery_look_at_archer_sponge_message);
+				end;
+			end;
+		end;
 		if (current_noun=NOUN_NONE) then
-			print_offset:=yellow_tree_dialog(yellow_tree_look_message);
+			print_offset:=archery_dialog(archery_look_message);
 		end; {verb_look}
 
+	VERB_PLAY:	begin
+		end; {verb_play}
+
+	VERB_TALK:	begin
+		{ only talk if close }
+
+		{ TODO: check y too? }
+		if (peasant_x<23) then begin
+			print_offset:=archery_dialog(archery_talk_too_far);
+		end;
+		else begin {close enough}
+			if (current_noun=NOUN_MAN) or
+				(current_noun=NOUN_GUY) or
+				(current_noun=NOUN_DUDE) or
+				(current_noun=NOUN_MENDELEV) or
+				(current_noun=NOUN_ARCHER) or
+				(current_noun=NOUN_NONE) then begin
+				{ talk to mendelev }
+				if (game_state.TALKED_TO_MENDELEV) then begin
+
+					{ URGH }
+				end;
+			end;
+			if (current_noun=NOUN_DONGOLEV) then begin
+
+			end;
+		end; { talk close }
+
+		end; {verb_talk}
 
 	end; {case verb}
 
